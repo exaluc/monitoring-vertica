6383f48540fa3ce33270a950c8186aa4f766c7b1
Lucian <odgon@users.noreply.github.com>
2020-06-29 10:51:26 +0200

    load config from json

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from fastapi import FastAPI
+import json
+
+with open('config.json') as jf:
+        d = json.load(jf)
+        vh = d['vertica']['host']
+        vpo = d['vertica']['port']
+        vu = d['vertica']['user']
+        vp = d['vertica']['password']
+        vd = d['vertica']['database']
 
 app = FastAPI()
 
@@ -6,3 +15,5 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+print(vh, vpo, vu, vp, vd)
