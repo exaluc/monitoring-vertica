36f81ce387a25d4d4d09d0298c62de36041698dc
Lucian <odgon@users.noreply.github.com>
2020-06-29 11:19:53 +0200

    vertica connection and query

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from vc import vc
 import json
 
 with open('config.json') as jf:
@@ -9,11 +10,32 @@ with open('config.json') as jf:
         vp = d['vertica']['password']
         vd = d['vertica']['database']
 
-app = FastAPI()
+class connection(vc):
+    ci = {'host': vh,
+             'port': vpo,
+             'user': vu,
+             'password': vp,
+             'database': vd,
+             'read_timeout': 100}
+    def go(self, query):
+        q = f'{query}'
+        self.query(q)
+        r = self.fetchall()
+        self.close()
+        return r
+        
 
+app = FastAPI(title="Monitoring Vertica")
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-print(vh, vpo, vu, vp, vd)
+@app.get("/query/{content}")
+def custom_query(content: str):
+    v = connection()
+    try:
+        r = v.go(content)
+    except Exception as e:
+        return {"error": e}
+    return {"data": r}
