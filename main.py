1f81f770146491d58bfa66146fc90514081a3bd6
Lucian <odgon@users.noreply.github.com>
2020-06-29 11:30:26 +0200

    System Health Tests

--- main.py
+++ main.py
@@ -23,19 +23,36 @@ class connection(vc):
         r = self.fetchall()
         self.close()
         return r
-        
 
 app = FastAPI(title="Monitoring Vertica")
 
-@app.get("/")
+@app.get("/", tags=["index"])
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/query/{content}")
+@app.get("/query/{content}", tags=["query"])
 def custom_query(content: str):
     v = connection()
     try:
         r = v.go(content)
+    except Exception as e:
+        return {"error": e}
+    return {"data": r}
+
+@app.get("/node/status", tags=["System Health"])
+def monitor_the_node_status():
+    v = connection()
+    try:
+        r = v.go("SELECT node_name, node_state FROM nodes ORDER BY 1;")
+    except Exception as e:
+        return {"error": e}
+    return {"data": r}
+
+@app.get("/epoch/status", tags=["System Health"])
+def monitor_the_epoch_status():
+    v = connection()
+    try:
+        r = v.go("SELECT current_epoch, ahm_epoch, last_good_epoch, designed_fault_tolerance, current_fault_tolerance, wos_used_bytes, ros_used_bytes FROM system;")
     except Exception as e:
         return {"error": e}
     return {"data": r}
